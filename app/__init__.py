1a13b4d6e4f0ce1706c396b2c1e0820d201532ba
Your Name <you@example.com>
2020-02-25 16:58:33 +0800

    添加登录、注册

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@
 # @File    : __init__.py.py
 # @Software: PyCharm
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -15,5 +16,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)#数据库对象
 migrate = Migrate(app, db)#迁移引擎对象
+login = LoginManager(app)
 
 from app import routes,models # 这里要写在app后面
