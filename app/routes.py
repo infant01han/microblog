39556888cdbcb49dd0e80cd72c9883a5776abe5e
Your Name <you@example.com>
2020-02-25 10:07:19 +0800

    add templates

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,23 @@
 # @Email   : hanlei5012@163.com
 # @File    : routes.py
 # @Software: PyCharm
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'hello'
+    usr = {'username':'zhangsan'}
+    posts = [#创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+        {
+            'author': {'username':'John'},
+            'body':'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username':'Susan'},
+            'body':'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',title='home',user=usr,posts=posts)
