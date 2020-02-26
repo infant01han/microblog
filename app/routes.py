f305a4b85c26c6830de3f27bda094cc344e8aebf
Your Name <you@example.com>
2020-02-26 10:31:57 +0800

    添加用户加载器(底层实现cookie)和用户退出功能

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,8 @@
 # @Email   : hanlei5012@163.com
 # @File    : routes.py
 # @Software: PyCharm
-from flask import render_template, flash, redirect, url_for
-from flask_login import login_user
+from flask import render_template, flash, redirect, url_for, session
+from flask_login import login_user, current_user, logout_user
 
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
@@ -15,7 +15,7 @@ from app.models import User
 @app.route('/')
 @app.route('/index')
 def index():
-    usr = {'username':'zhangsan'}
+    # usr = {'username':'zhangsan'}
     posts = [#创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
         {
             'author': {'username':'John'},
@@ -26,10 +26,12 @@ def index():
             'body':'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html',title='home',user=usr,posts=posts)
+    return render_template('index.html',title='home',posts=posts)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -52,3 +54,12 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html',title='Register',form=form)
+# @app.route('/hello')
+# def hello():
+#     session['username']='lisi'
+#     return 'hello'
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
